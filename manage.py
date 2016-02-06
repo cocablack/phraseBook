b0d006a1d22e18930d28e37315fe45b0ef15d622
cocablack <jiyun_lee@brown.edu>
2016-02-06 17:55:35 -0500

    simple mongodb stuff

--- manage.py
+++ manage.py
@@ -1,29 +1,18 @@
-import datetime
-from flask import url_for
-from tumblelog import db
+# Set the path
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from flask.ext.script import Manager, Server
+from tumblelog import app
 
-class Post(db.Document):
-    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
-    title = db.StringField(max_length=255, required=True)
-    slug = db.StringField(max_length=255, required=True)
-    body = db.StringField(required=True)
-    comments = db.ListField(db.EmbeddedDocumentField('Comment'))
+manager = Manager(app)
 
-    def get_absolute_url(self):
-        return url_for('post', kwargs={"slug": self.slug})
+# Turn on debugger by default and reloader
+manager.add_command("runserver", Server(
+    use_debugger = True,
+    use_reloader = True,
+    host = '0.0.0.0')
+)
 
-    def __unicode__(self):
-        return self.title
-
-    meta = {
-        'allow_inheritance': True,
-        'indexes': ['-created_at', 'slug'],
-        'ordering': ['-created_at']
-    }
-
-
-class Comment(db.EmbeddedDocument):
-    created_at = db.DateTimeField(default=datetime.datetime.now, required=True)
-    body = db.StringField(verbose_name="Comment", required=True)
-    author = db.StringField(verbose_name="Name", max_length=255, required=True)
+if __name__ == "__main__":
+    manager.run()
