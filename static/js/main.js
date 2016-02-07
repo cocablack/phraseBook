b6cb50ed42707c9adc2cd9c35391a53e6df651a5
Ali Mir <ali.ahmed.mir@gmail.com>
2016-02-07 11:55:42 -0500

    James

--- static/js/main.js
+++ static/js/main.js
@@ -27,8 +27,23 @@ function query(json_data) {
         dataType: "text",
         success: function(data) {
             console.log("success");
-            var resp = JSON.parse(data);
+            var resp = JSON.parse(JSON.parse(data));
             console.log(resp);
+
+            Window.hi = resp;
+            Window.ho = data;
+
+            $("#inputBox").css("display", "none");
+
+            var author = resp[2 ];
+
+            console.log(resp)
+            console.log(author)
+
+            $("#sentiment_text").text( author );
+            $("#outputBox").css("display", "block");
+
+
         },
         error: function(data) {
             console.log("something went wrong");
